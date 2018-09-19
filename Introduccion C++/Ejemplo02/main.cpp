f2e1331475537df6fb69f41c66b804431b901322
javiergualix <36969030+javiergualix@users.noreply.github.com>
2018-09-19 05:22:19 +0200

    Ejemplo02
    Terminado el Ejemplo 02

---
+++ Introduccion C++/Ejemplo02/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main( )
+{
+	int x, y, z;
+	cout << "Introduzca el primer numero: ";
+	cin >> x;
+	cout << "Introduzca el segundo numero: ";
+	cin >> y;
+	z = x + y;
+	cout << "La suma " << x << " + " << y << " = " << z << endl;
+	
+	return 0;
+}
