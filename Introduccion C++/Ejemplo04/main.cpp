1cf8f8a090e2c33d059473abfcf00356f5b7d104
javiergualix <36969030+javiergualix@users.noreply.github.com>
2018-09-19 05:29:50 +0200

    Ejemplo04
    Terminado el Ejemplo 04

---
+++ Introduccion C++/Ejemplo04/main.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main( )
+{
+	int x, y, min, max;
+	cout << "Valor de x: ";
+	cin >> x;
+	cout << "Valor de y: ";
+	cin >> y;
+	
+	if (x < y)
+	{
+		min = x;
+	}
+	else
+	{
+		min = y;
+	}
+	
+	max = (x > y ? x : y);
+	
+	cout << "El minimo es " << min << " y el maximo es " << max << endl;
+	
+	return 0;
+}
